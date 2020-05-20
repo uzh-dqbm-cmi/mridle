b2b7ee3278a8ffbba21fed09a4404ad00266c3f0
lokijuhy <laura.kinkead@uzh.ch>
2020-05-20 15:51:41 +0200

    add model_eval to init imports

--- mridle/__init__.py
+++ mridle/__init__.py
@@ -3,6 +3,8 @@ from mridle import exploration_utilities
 from mridle import plotting_utilities
 from mridle import feature_engineering
 from mridle import experiment
+from mridle import model_evaluation
 
 
-__all__ = ['data_management', 'exploration_utilities', 'plotting_utilities', 'feature_engineering', 'experiment']
+__all__ = ['data_management', 'exploration_utilities', 'plotting_utilities', 'feature_engineering', 'experiment',
+           'model_evaluation']
