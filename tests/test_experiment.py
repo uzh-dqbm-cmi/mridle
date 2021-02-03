d72909a00c8768c7d47e14ed5c8946091a01f723
nicoapg <40054301+nicoapg@users.noreply.github.com>
2021-02-03 21:20:57 +0100

    Update test_experiment.py

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -9,7 +9,10 @@ from sklearn.ensemble import RandomForestClassifier
 class TestExperiment(unittest.TestCase):
 
     def test_integration_test(self):
-        columns = ['historic_no_show_cnt', 'days_sched_in_advance', 'sched_for_hour', 'no_show_before']
+        #columns = ['historic_no_show_cnt', 'days_sched_in_advance', 'sched_for_hour', 'no_show_before']
+        cols_for_modeling = ['no_show_before', 'no_show_before_sq', 'sched_days_advanced', 'hour_sched',
+                     'distance_to_usz', 'age', 'close_to_usz', 'male', 'female', 'age_sq',
+                     'sched_days_advanced_sq', 'distance_to_usz_sq', 'sched_2_days', 'age_20_60']
         df = pd.DataFrame(np.random.randint(0, 100, size=(100, len(columns))), columns=columns)
         df['noshow'] = np.where(df['historic_no_show_cnt'] > 50, 1, 0)
         harvey_model_run = HarveyModel
