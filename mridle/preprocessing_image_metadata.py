e25c340ac868986ec44ba9dc0e9cda16ddf1d84a
Mark McMahon <mcmahom5@tcd.ie>
2021-05-06 17:14:11 +0200

    added docstrings

--- mridle/preprocessing_image_metadata.py
+++ mridle/preprocessing_image_metadata.py
@@ -1,52 +1,135 @@
+"""
+Code to process the data extracts from Marc Bovet at the RDSC. This data extract comes with each appointment having
+multiple rows associated with it, each row representing an image scanned by the MRI machine. We receive just a subset
+of the metadata related to this image, and no actual image file.
+
+The below functions are used in the data preprocessing step(s), and individual descriptions are given at the start
+of each function.
+"""
+
 import datetime as dt
 import numpy as np
 import pandas as pd
 
 
-def clean_marc_extract(df):
-    # df = df[['AccessionNumber', 'AcquisitionDate', 'AcquisitionTime', 'DeviceSerialNumber', 'StationName', 'SeriesUID', 'StudyUID']].copy()
-    df['AcquisitionDate'] = pd.to_datetime(df['AcquisitionDate'], format='%Y%m%d').dt.date
-    df['AcquisitionTime'] = pd.to_datetime(df['AcquisitionTime'], format='%H%M%S.%f').dt.time
-    df.loc[~df['AcquisitionDate'].isnull(), 'acq_week'] = df.loc[
-        ~df['AcquisitionDate'].isnull(), 'AcquisitionDate'].apply(lambda x: x.isocalendar().week)
-    df = df.drop_duplicates()
-    df = df[~df['AccessionNumber'].isna()]
-    df = df[~df['AcquisitionDate'].isnull()]
-    df.loc[~df['AcquisitionTime'].isnull(), 'acq_datetime'] = df[~df['AcquisitionTime'].isnull()].apply(
+def process_date_cols(df: pd.DataFrame):
+    """
+    Take in the dataframe provided by Marc and add some date columns for easier calculations later
+
+    Args:
+        df: Dataframe where each row represents one image for an appointment,
+         containing columns: ['AcquisitionDate', 'AcquisitionTime']
+
+    Returns:
+        dataframe with some reformatted date columns, and some new columns added
+    """
+    df_copy = df.copy()
+    df_copy['AcquisitionDate'] = pd.to_datetime(df_copy['AcquisitionDate'], format='%Y%m%d').dt.date
+    df_copy['AcquisitionTime'] = pd.to_datetime(df_copy['AcquisitionTime'], format='%H%M%S.%f').dt.time
+    df_copy.loc[~df_copy['AcquisitionDate'].isnull(), 'acq_week'] = df_copy.loc[
+        ~df_copy['AcquisitionDate'].isnull(), 'AcquisitionDate'].apply(lambda x: x.isocalendar().week)
+    df_copy.loc[~df_copy['AcquisitionTime'].isnull(), 'acq_datetime'] = df_copy[~df_copy['AcquisitionTime'].isnull()].apply(
         lambda x: dt.datetime.combine(x['AcquisitionDate'], x['AcquisitionTime']), axis=1)
 
-    return df
+    return df_copy
+
+
+def remove_na_and_duplicates(df: pd.DataFrame):
+    """
+    Removes duplicate rows and removes appointments with no AccessionNumber (appointment ID) or no data given
+
+    NOTE: This AccessionNumber being null 'issue' is currently being checked by Marc Bovet, so perhaps this will become
+    redundant in the future.
+
+    Args:
+        df: Dataframe containing columns ['AccessionNumber', 'AcquisitionDate']
+    Returns:
+        Dataframe with duplicates removed and rows with null AccessionNumber or AcquisitionData columns
+
+    """
+    df_copy = df.copy()
+    df_copy = df_copy.drop_duplicates()
+    df_copy = df_copy[~df_copy['AccessionNumber'].isna()]
+    df_copy = df_copy[~df_copy['AcquisitionDate'].isnull()]
+
+    return df_copy
 
 
 def get_image_time_cols(df):
-    df = df.sort_values(['AccessionNumber', 'acq_datetime'])
-    df['AcquisitionTime_prev'] = df.groupby('AccessionNumber')['AcquisitionTime'].shift(1)
-    df['acq_prev_datetime'] = df.groupby('AccessionNumber')['acq_datetime'].shift(1)
-    df['acq_next_datetime'] = df.groupby('AccessionNumber')['acq_datetime'].shift(-1)
+    """
+    Adds some useful columns to the dataframe that are used later in calculations and data processing.
 
-    df['img_rank'] = df.groupby('AccessionNumber')['acq_datetime'].transform('rank', ascending=True)
-    df['img_rank_rev'] = df.groupby('AccessionNumber')['acq_datetime'].transform('rank', ascending=False)
+    The first group of these columns is time information for the previous and the next image in the sequence of images
+    per appointment. Also added are columns calculating the time between the previous image in the appointment and the
+    row image, as well as the time between the row image and the next image. (row image meaning the image that the
+    given row is related to). Finally for this group, a flag is added for each image in the appointment if two images
+    in the sequence are more than 1800 seconds / 30 minutes apart.
 
-    df['time_between_next_image'] = (df['acq_next_datetime'] - df['acq_datetime']) / pd.to_timedelta(1, unit='S')
-    df['time_between_prev_image'] = (df['acq_datetime'] - df['acq_prev_datetime']) / pd.to_timedelta(1, unit='S')
+    The second group of columns is the 'rank' of the image, or what position it is in the sequence of images for the
+    appointment (i.e. is it the first image, second, ..., 20th, ..., second last, last). And the same column idea
+    is provided in a separate column, but with the rank in reverse order - this is mostly for easier calculations later
+    and to make it clear which images are really the last (rather than just showing that they are the 254th image in a
+    sequence of 254 images).
 
-    df['big_image_gap'] = np.max(df[['time_between_next_image', 'time_between_prev_image']], axis=1)
-    df['big_image_gap'] = df[['AccessionNumber', 'big_image_gap']].groupby('AccessionNumber').transform(
+    Args:
+        df: Dataframe where each row represents one image for an appointment,
+         containing columns: ['AccessionNumber', 'AcquisitionTime', 'acq_datetime']
+
+    Returns:
+        Dataframe with the columns as described above added.
+
+    """
+    df_copy = df.copy()
+    df_copy = df_copy.sort_values(['AccessionNumber', 'acq_datetime'])
+    df_copy['AcquisitionTime_prev'] = df_copy.groupby('AccessionNumber')['AcquisitionTime'].shift(1)
+    df_copy['acq_prev_datetime'] = df_copy.groupby('AccessionNumber')['acq_datetime'].shift(1)
+    df_copy['acq_next_datetime'] = df_copy.groupby('AccessionNumber')['acq_datetime'].shift(-1)
+
+    df_copy['time_between_next_image'] = (df_copy['acq_next_datetime'] - df_copy['acq_datetime']) / pd.to_timedelta(1, unit='S')
+    df_copy['time_between_prev_image'] = (df_copy['acq_datetime'] - df_copy['acq_prev_datetime']) / pd.to_timedelta(1, unit='S')
+
+    df_copy['big_image_gap'] = np.max(df_copy[['time_between_next_image', 'time_between_prev_image']], axis=1)
+    df_copy['big_image_gap'] = df_copy[['AccessionNumber', 'big_image_gap']].groupby('AccessionNumber').transform(
         lambda x: 1 if np.max(x) > 1800 else 0)
 
-    return df
+    df_copy['img_rank'] = df_copy.groupby('AccessionNumber')['acq_datetime'].transform('rank', ascending=True)
+    df_copy['img_rank_rev'] = df_copy.groupby('AccessionNumber')['acq_datetime'].transform('rank', ascending=False)
+
+    return df_copy
 
 
-def remove_start_end_images(df, longer_than_secs=1800, within_first_last=5):
-    remove_before = df.loc[(df['img_rank'] <= within_first_last) &
-                           (df['time_between_next_image'] > longer_than_secs), ["AccessionNumber", "img_rank"]]
-    remove_after = df.loc[(df['img_rank_rev'] <= within_first_last) &
-                          (df['time_between_prev_image'] > longer_than_secs), ["AccessionNumber", "img_rank_rev"]]
+def remove_start_end_images(df):
+    """
+    Take in image metadata from Marc's (RDSC) extract and remove images that are within the first or last 5 images of
+    the appointment, AND where there is a large gap (default of 30 minutes, 1800 seconds) contained within the first
+    or last 5 images of the sequence.
+
+    e.g. if there are 3 images taken at ~9am, and then a gap of 90 minutes until the remaining images are taken as part
+    of this AccessionNumber (could be 100+ images), we remove these 3 images at the start, leaving behind only the
+    'true' section of the appointment. It is assumed that these original 3 images were created as a computer error,
+    and are not genuinely relating to the appointment.
+
+    - Requires columns that are added by the get_image_time_cols() function
+
+    Args:
+        df: Dataframe with multiple rows per AccessionNumber (each row is the metadata for an individual image)
+
+    Returns:
+        Dataframe with rows removed as necessary.
+
+    """
+    df_copy = df.copy()
+    remove_before = df_copy.loc[(df_copy['img_rank'] <= 5) & (df_copy['time_between_next_image'] > 1800),
+                           ["AccessionNumber", "img_rank"]]
+    remove_after = df_copy.loc[(df_copy['img_rank_rev'] <= 5) & (df_copy['time_between_prev_image'] > 1800),
+                          ["AccessionNumber", "img_rank_rev"]]
 
     for idx, row in remove_before.iterrows():
-        df = df[~((df['img_rank'] <= row['img_rank']) & (df['AccessionNumber'] == row['AccessionNumber']))]
+        df_copy = df_copy[~((df_copy['img_rank'] <= row['img_rank']) &
+                            (df_copy['AccessionNumber'] == row['AccessionNumber']))]
 
     for idx, row in remove_after.iterrows():
-        df = df[~((df['img_rank_rev'] <= row['img_rank_rev']) & (df['AccessionNumber'] == row['AccessionNumber']))]
+        df_copy = df_copy[~((df_copy['img_rank_rev'] <= row['img_rank_rev']) &
+                            (df_copy['AccessionNumber'] == row['AccessionNumber']))]
 
-    return df
+    return df_copy
