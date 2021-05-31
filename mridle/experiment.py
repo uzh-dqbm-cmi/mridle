3e642d92bc5bb53f25fda06f322a0df717331a87
Mark McMahon <mcmahom5@tcd.ie>
2021-05-31 14:21:23 +0200

    Linting

--- mridle/experiment.py
+++ mridle/experiment.py
@@ -97,8 +97,8 @@ class ModelRun:
 
     @classmethod
     def build_data(cls, train_set: Any, test_set: Any, label_key: str,
-                   feature_subset: List[str], cat_columns: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame, List, List, List[str],
-                                                       Dict[str, Any]]:
+                   feature_subset: List[str], cat_columns: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame, List, List,
+                                                                               List[str], Dict[str, Any]]:
         """Orchestrates the construction of train and test x matrices, and train and test y vectors.
 
         `build_data` takes as input:
