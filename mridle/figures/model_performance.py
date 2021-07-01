bce1a9718db71e183a2bede3773cb5ef7b644a70
Mark McMahon <mcmahom5@tcd.ie>
2021-07-01 09:34:02 +0200

    Changing docstring format

--- mridle/figures/model_performance.py
+++ mridle/figures/model_performance.py
@@ -6,13 +6,16 @@ def plot_num_preds_vs_precision(results_df: pd.DataFrame, n_preds_col: str = '# 
                                 precision_col: str = 'PPV / Precision', name_col: str = 'model_name') -> alt.Chart:
     """
     Taking in a dataframe containing evaluation results achieved by each model, produce a scatter plot with the
-     number of positive predictions on the X-axis, and the precision on the Y-axis
+    number of positive predictions on the X-axis, and the precision on the Y-axis
 
-    :param results_df: dataframe of model evaluations on a test set
-    :param n_preds_col: column name containing the number of positive predictions a model gives
-    :param precision_col: column name containing the precision a model achieves
-    :param name_col: column name containing the name of the model to be used in the plot legend
-    :return: altair scatter plot with number of positive predictions on the X-axis, and the precision on the Y-axis
+    Args:
+        results_df: dataframe of model evaluations on a test set
+        n_preds_col: column name containing the number of positive predictions a model gives
+        precision_col: column name containing the precision a model achieves
+        name_col: column name containing the name of the model to be used in the plot legend
+
+    Returns:altair scatter plot with number of positive predictions on the X-axis, and the precision on the Y-axis
+
     """
     return alt.Chart(results_df[[n_preds_col, precision_col, name_col]]).mark_circle(size=60, opacity=1).encode(
         x=n_preds_col,
