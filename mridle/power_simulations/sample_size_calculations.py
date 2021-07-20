9cc088d500c2e85f6a4cc0a511edf00430a5e969
Mark McMahon <mcmahom5@tcd.ie>
2021-07-20 14:23:55 +0100

    Changing to proper number of simulations

--- mridle/power_simulations/sample_size_calculations.py
+++ mridle/power_simulations/sample_size_calculations.py
@@ -10,8 +10,8 @@ effect_sizes = [0.1, 0.15, 0.2]
 sample_sizes = [avg_appts_per_week * (i+1) for i in range(10)]
 
 
-exp = PowerSimulations(sample_sizes=sample_sizes, effect_sizes=effect_sizes, num_trials_per_run=1,
-                       num_runs_for_power_calc=1, original_test_set_length=4000, significance_level=0.05,
+exp = PowerSimulations(sample_sizes=sample_sizes, effect_sizes=effect_sizes, num_trials_per_run=1000,
+                       num_runs_for_power_calc=1000, original_test_set_length=4000, significance_level=0.05,
                        base_recall=recall, base_precision=precision, num_cpus=8, random_seed=0)
 
 exp.run()
