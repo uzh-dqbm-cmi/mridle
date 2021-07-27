d0870613b15b5a48bc4e320b60a1419b4870687f
mcmahom5 <mcmahom5@tcd.ie>
2021-07-27 15:13:00 +0200

    Update tests/test_power_simulations.py
    Co-authored-by: Laura <lokijuhy@users.noreply.github.com>

--- tests/test_power_simulations.py
+++ tests/test_power_simulations.py
@@ -71,11 +71,7 @@ class TestPowerSimulations(unittest.TestCase):
         self.assertTrue(test_condition)
 
     def test_f1_same_df(self):
-        exp = PowerSimulations(sample_sizes=sample_sizes, effect_sizes=effect_sizes, num_trials_per_run=1000,
-                               num_runs_for_power_calc=1000, original_test_set_length=4000, significance_level=0.05,
-                               base_recall=recall, base_precision=precision, num_cpus=8, random_seed=0)
-
-        df = exp.generate_actuals_preds(exp.base_precision, exp.base_recall, exp.original_test_set_length)
+        df = PowerSimulations.generate_actuals_preds(exp.base_precision, exp.base_recall, exp.original_test_set_length)
 
         score = calculate_f1_diff(df, df)
         self.assertEqual(score, 0)
