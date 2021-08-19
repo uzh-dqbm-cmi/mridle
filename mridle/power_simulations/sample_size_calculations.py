fef8f0964b042f29fac63c2e0fff3836143abec0
Mark McMahon <mcmahom5@tcd.ie>
2021-08-19 09:56:32 +0200

    Updating sample_size_calculations.py

--- mridle/power_simulations/sample_size_calculations.py
+++ mridle/power_simulations/sample_size_calculations.py
@@ -10,8 +10,8 @@ effect_sizes = [0.1, 0.15, 0.2]
 sample_sizes = [avg_appts_per_week * (i+1) for i in range(10)]
 
 
-exp = PowerSimulations(sample_sizes=sample_sizes, effect_sizes=effect_sizes, num_trials_per_run=10,
-                       num_runs_for_power_calc=10, original_test_set_length=4000, significance_level=0.05,
+exp = PowerSimulations(sample_sizes=sample_sizes, effect_sizes=effect_sizes, num_trials_per_run=1000,
+                       num_runs_for_power_calc=1000, original_test_set_length=4000, significance_level=0.05,
                        base_performance=performance, performance_type=performance_type, num_cpus=8, random_seed=0)
 exp.generate_actuals_preds(performance, 1000)
 exp.run()
