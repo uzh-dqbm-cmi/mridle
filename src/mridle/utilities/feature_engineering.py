e11cb1622140d51f61a982372bafe07ff2076988
mcmahom5 <mcmahom5@tcd.ie>
2021-09-22 10:53:01 +0200

    Update feature_engineering.py
    Updating import (again)

--- src/mridle/utilities/feature_engineering.py
+++ src/mridle/utilities/feature_engineering.py
@@ -2,7 +2,7 @@ import datetime as dt
 import pandas as pd
 import numpy as np
 import pgeocode
-from mridle.utilities.data_management import build_slot_df
+from mridle.pipelines.data_engineering.ris.nodes import build_slot_df
 
 
 def identify_end_times(row: pd.DataFrame) -> dt.datetime:
