5f8aed07800f70b206327148f00dfb7fff20c261
mcmahom5 <mcmahom5@tcd.ie>
2021-09-22 10:49:18 +0200

    Update feature_engineering.py
    Typo fix

--- src/mridle/utilities/feature_engineering.py
+++ src/mridle/utilities/feature_engineering.py
@@ -2,7 +2,7 @@ import datetime as dt
 import pandas as pd
 import numpy as np
 import pgeocode
-from mridle.utlities.data_management import build_slot_df
+from mridle.utilities.data_management import build_slot_df
 
 
 def identify_end_times(row: pd.DataFrame) -> dt.datetime:
