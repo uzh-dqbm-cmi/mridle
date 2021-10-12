fe3d4485576c08ce193cc459b7e4717a99bf0acc
lokijuhy <laura.kinkead@uzh.ch>
2021-10-12 11:39:32 +0200

    add dispo exp 1

--- src/mridle/pipelines/data_engineering/dispo/pipeline.py
+++ src/mridle/pipelines/data_engineering/dispo/pipeline.py
@@ -1,23 +1,33 @@
 from kedro.pipeline import Pipeline, node
-from .nodes import build_dispo_exp_2_df, calc_exp_confusion_matrix, calc_jaccard_score_table
+from .nodes import build_dispo_exp_1_df, build_dispo_exp_2_df, calc_exp_confusion_matrix, calc_jaccard_score_table
 from ..ris.nodes import build_status_df, build_slot_df
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
-            # node(
-            #     func=lambda part_1, part_2: part_1 + part_2,
-            #     inputs=["experiment1A", "experiment1B"],
-            #     outputs="experiment1_records",
-            #     name="concat_raw_exp_1_records",
-            # ),
-            # node(
-            #     func=build_dispo_exp_1_df,
-            #     inputs="experiment1_records",
-            #     outputs="dispo_exp_1_df",
-            #     name="build_dispo_exp_1_df",
-            # ),
+            # EXPERIMENT 1 - DEPRECATED
+            node(
+                func=lambda part_1, part_2: part_1 + part_2,
+                inputs=["experiment1A", "experiment1B"],
+                outputs="experiment1_records",
+                name="concat_raw_exp_1_records",
+            ),
+            node(
+                func=build_dispo_exp_1_df,
+                inputs="experiment1_records",
+                outputs="dispo_exp_1_df",
+                name="build_dispo_exp_1_df",
+            ),
+            node(
+                func=calc_exp_confusion_matrix,
+                inputs=["dispo_exp_1_df", "slot_df"],
+                outputs="val_exp_1_confusion_matrix",
+                name="calc_exp_1_confusion_matrix"
+
+            ),
+
+            # EXPERIMENT 2 - DEVELOPMENT
             node(
                 func=lambda part_1, part_2: part_1 + part_2,
                 inputs=["experiment2", "experiment2_corrections"],
@@ -30,6 +40,20 @@ def create_pipeline(**kwargs):
                 outputs="dispo_development_slot_df",
                 name="build_dispo_exp_2_df"
             ),
+            node(
+                func=build_status_df,
+                inputs=["val_ris_development", "test_patient_ids"],
+                outputs="val_ris_development_status_df",
+                name="build_val_ris_development_status_df"
+            ),
+            node(
+                func=build_slot_df,
+                inputs="val_ris_development_status_df",
+                outputs="val_ris_development_slot_df",
+                name="build_val_ris_development_slot_df"
+            ),
+
+            # EXPERIMENT 3 - EVALUATION
             node(
                 func=lambda part_1, part_2: part_1 + part_2,
                 inputs=["experiment3", "experiment3_corrections"],
@@ -44,18 +68,6 @@ def create_pipeline(**kwargs):
             ),
             node(
                 func=build_status_df,
-                inputs=["val_ris_development", "test_patient_ids"],
-                outputs="val_ris_development_status_df",
-                name="build_val_ris_development_status_df"
-            ),
-            node(
-                func=build_slot_df,
-                inputs="val_ris_development_status_df",
-                outputs="val_ris_development_slot_df",
-                name="build_val_ris_development_slot_df"
-            ),
-            node(
-                func=build_status_df,
                 inputs=["val_ris_evaluation", "test_patient_ids"],
                 outputs="val_ris_evaluation_status_df",
                 name="build_val_ris_evaluation_status_df"
@@ -66,6 +78,8 @@ def create_pipeline(**kwargs):
                 outputs="val_ris_evaluation_slot_df",
                 name="build_val_ris_evaluation_slot_df"
             ),
+
+            # REPORTING
             node(
                 func=calc_jaccard_score_table,
                 inputs=["dispo_development_slot_df", "val_ris_development_slot_df", "dispo_evaluation_slot_df",
