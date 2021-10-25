e58812f03f32a80776444195cf3d76d8d40cae73
Mark McMahon <mcmahom5@tcd.ie>
2021-10-25 14:36:59 +0200

    More dates

--- src/mridle/pipelines/data_engineering/dicom/nodes.py
+++ src/mridle/pipelines/data_engineering/dicom/nodes.py
@@ -606,8 +606,8 @@ def calc_idle_time_gaps(dicom_times_df: pd.DataFrame, tp_agg_df: pd.DataFrame, t
     # Join on terminplanner data
     idle_df = idle_df.merge(tp_agg_df, how='left', on=['day_of_week', 'image_device_id'])
 
-    idle_df['day_start_tp'] = pd.to_datetime(idle_df['day_start_tp'], format='%H:%M').dt.time
-    idle_df['day_end_tp'] = pd.to_datetime(idle_df['day_end_tp'], format='%H:%M').dt.time
+    idle_df['day_start_tp'] = pd.to_datetime(idle_df['day_start_tp'], format='%H:%M:%S').dt.time
+    idle_df['day_end_tp'] = pd.to_datetime(idle_df['day_end_tp'], format='%H:%M:%S').dt.time
 
     idle_df['day_start_tp'] = idle_df.apply(lambda x: datetime.datetime.combine(x['date'], x['day_start_tp']), axis=1)
     idle_df['day_end_tp'] = idle_df.apply(lambda x: datetime.datetime.combine(x['date'], x['day_start_tp']), axis=1)
