288b0eaf46442ba5aecc25ce1863039cc51977b9
Mark McMahon <mcmahom5@tcd.ie>
2021-11-10 12:12:43 +0100

    Adding partition number to print

--- src/mridle/experiment/experiment.py
+++ src/mridle/experiment/experiment.py
@@ -36,8 +36,8 @@ class Experiment:
 
     def go(self):
         self.run_date = datetime.datetime.now()
-        for x_train, y_train, x_test, y_test in self.stratifier:
-            print('Running partition...')
+        for i, (x_train, y_train, x_test, y_test) in enumerate(self.stratifier):
+            print('Running partition {}...'.format(i+1))
             predictor = self.trainer.fit(x_train, y_train)
             self.partition_predictors.append(predictor)
             partition_evaluation = self.evaluate(predictor, self.metrics, x_test, y_test)
