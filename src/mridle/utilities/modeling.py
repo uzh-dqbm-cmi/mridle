3f61f868d488a3c2d6759e0263fffe6f5af5fb0d
Mark McMahon <mcmahom5@tcd.ie>
2021-11-11 16:12:35 +0100

    First start

--- src/mridle/utilities/modeling.py
+++ src/mridle/utilities/modeling.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from typing import Dict, Tuple
 from mridle.experiment.experiment import Experiment
+from hyperopt import hp
 
 
 def parse_hyperparams(hyperparams: Dict) -> Dict:
@@ -10,6 +11,13 @@ def parse_hyperparams(hyperparams: Dict) -> Dict:
             solo_key = list(hyperparams[entry].keys())[0]
             if solo_key == 'parse_np_linspace':
                 hyperparams[entry] = [int(x) for x in np.linspace(**hyperparams[entry]['parse_np_linspace'])]
+            if solo_key == 'parse_hp_choice':
+                # parse_hp_choice
+                #   name: 'num_estimators'
+                #   choice_list: [10, 100, 400]
+                hyperparams[entry] = hp.choice(hyperparams[entry]['parse_np_linspace']['name'],
+                                               hyperparams[entry]['parse_np_linspace']['choice_list'])
+
     return hyperparams
 
 
