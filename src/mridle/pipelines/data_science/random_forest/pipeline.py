f8201dca17aaaf4cde89bf218cb90de7e5f076ad
Mark McMahon <mcmahom5@tcd.ie>
2021-11-15 16:17:53 +0100

    Adding remove_na function for now

--- src/mridle/pipelines/data_science/random_forest/pipeline.py
+++ src/mridle/pipelines/data_science/random_forest/pipeline.py
@@ -1,13 +1,21 @@
 from kedro.pipeline import Pipeline, node
 from mridle.utilities.modeling import run_experiment
+from .nodes import process_features_for_model
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
             node(
+                func=process_features_for_model,
+                inputs=["master_feature_set"],
+                outputs="model_input",
+                name="process_features_for_harvey_model"
+            ),
+
+            node(
                 func=run_experiment,
-                inputs=["master_feature_set", "params:models.random_forest"],
+                inputs=["model_input", "params:models.random_forest"],
                 outputs=["random_forest_model", "random_forest_model_results"],
                 name="train_harvey_model_random_forest"
             )
