bac20ccb142ddc3df9dd8b3de876e2b9b9b0a537
Mark McMahon <mcmahom5@tcd.ie>
2021-11-24 14:13:08 +0100

    Fixes

--- src/mridle/pipelines/data_science/neural_net/pipeline.py
+++ src/mridle/pipelines/data_science/neural_net/pipeline.py
@@ -1,13 +1,20 @@
 from kedro.pipeline import Pipeline, node
 from mridle.utilities.modeling import run_experiment
+from .nodes import remove_na
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
             node(
+                func=remove_na,
+                inputs=["master_feature_set"],
+                outputs="input_feature_set",
+                name="remove_na"
+            ),
+            node(
                 func=run_experiment,
-                inputs=["train_data", "params:models.neural_net"],
+                inputs=["input_feature_set", "params:models.neural_net"],
                 outputs=["neural_net_model", "neural_net_model_results"],
                 name="train_neural_net_model"
             )
