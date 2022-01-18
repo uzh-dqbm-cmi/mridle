6d88caf3fd281538283588458a40687b77636712
Mark McMahon <mcmahom5@tcd.ie>
2022-01-18 14:53:03 +0100

    Params

--- src/mridle/utilities/power_simulations/sample_size_calculations.py
+++ src/mridle/utilities/power_simulations/sample_size_calculations.py
@@ -6,13 +6,13 @@ performance_type = 'log_loss'
 avg_appts_per_week = 166  # taken from aggregation of df_features_original data for the year 2017 (in notebook #52)
 
 
-effect_sizes = [0.05, 0.075, 0.1]
+effect_sizes = [0.01, 0.05, 0.075]
 sample_sizes = [avg_appts_per_week * (i+1) for i in range(10)]
 
 
 exp = PowerSimulations(sample_sizes=sample_sizes, effect_sizes=effect_sizes, num_trials_per_run=1000,
                        num_runs_for_power_calc=1000, original_test_set_length=6536, significance_level=0.05,
-                       base_performance=performance, performance_type=performance_type, num_cpus=1, random_seed=0)
+                       base_performance=performance, performance_type=performance_type, num_cpus=8, random_seed=0)
 
 exp.run()
 exp.save(parent_directory=os.getcwd())
