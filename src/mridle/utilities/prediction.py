005caaeac98504b3f22f8f3f58314f748caa6c22
Mark McMahon <mcmahom5@tcd.ie>
2022-06-20 16:52:18 +0200

    Changes

--- src/mridle/utilities/prediction.py
+++ src/mridle/utilities/prediction.py
@@ -29,10 +29,9 @@ def main(data_path, model_dir, output_path, valid_date_range, file_encoding, mas
 
     exclude_pat_ids = list()  # TODO!
     print(raw_df[raw_df['MRNCmpdId'].str.contains('_')].head())
-    raw_df['MRNCmpdId'] = raw_df['MRNCmpdId'].str.replace('_', '.')  # because some MRNCmpdIds have leading underscores,
+    raw_df['MRNCmpdId'] = raw_df['MRNCmpdId'].str.replace('_', '')  # because some MRNCmpdIds have leading underscores,
     # which usually denotes a test appointment and is thus removed, but in silent live test and with 'future' data,
     # sometimes the MRNCmpdId has a leading underscore which is later removed/changed to a 'proper' MRNCmpdId
-    print(raw_df[raw_df['MRNCmpdId'].str.contains('_')].head())
     formatted_df = prep_raw_df_for_parquet(raw_df)
     status_df = build_status_df(formatted_df, exclude_pat_ids)
     status_df = status_df.merge(rfs_df, how='left')
