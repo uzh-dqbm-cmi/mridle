590af7a31216f62534909e754b374eed2c93032f
Mark McMahon <mcmahom5@tcd.ie>
2022-06-21 10:55:34 +0200

    Changes

--- src/mridle/pipelines/data_science/feature_engineering/nodes.py
+++ src/mridle/pipelines/data_science/feature_engineering/nodes.py
@@ -304,10 +304,11 @@ def feature_marital(status_df: pd.DataFrame) -> pd.DataFrame:
         # 'LV': 'life partnership dissolved by death',
         # 'LA': 'forcible partnership',
         # 'LE': 'civil partnership dissolved by declaration of death',
-        np.NaN: 'blank',
+        np.NaN: 'not known',
     }
 
     status_df['marital'] = status_df['Zivilstand'].map(zivilstand_abbreviation_mapping)
+    status_df['marital'].fillna('not known', inplace=True)
     return status_df
 
 
