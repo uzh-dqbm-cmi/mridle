37c283aaf090a54ba35e6dcd913df0ff6cb550e6
Mark McMahon <mcmahom5@tcd.ie>
2023-02-07 17:04:34 +0100

    Pipeline

--- src/mridle/pipelines/data_science/live_data/nodes.py
+++ src/mridle/pipelines/data_science/live_data/nodes.py
@@ -34,6 +34,6 @@ def concat_master_data(master_feature_set_na_removed, live_data):
     val_data_with_live = live_data[live_data['start_time'].dt.date >= five_weeks_ago]
 
     train_data_with_live = pd.concat([master_feature_set_na_removed, live_data_train], join="inner")
-    train_data_with_live['post_code'] = train_data_with_live['post_code'].astype(str)
+    train_data_with_live['post_code'] = train_data_with_live['post_code'].astype(int)
 
     return train_data_with_live, val_data_with_live
