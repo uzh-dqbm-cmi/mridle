aef08bdce820a571875d69c6a618fb1b88a244e3
Mark McMahon <mcmahom5@tcd.ie>
2023-02-07 17:16:11 +0100

    Pipeline

--- src/mridle/pipelines/data_science/live_data/nodes.py
+++ src/mridle/pipelines/data_science/live_data/nodes.py
@@ -26,7 +26,7 @@ def get_slt_with_outcome():
 def concat_master_data(master_feature_set_na_removed, live_data):
     """Take live data up until start of last month, and concat with master feature set. That is then training data.
     Rest of live data (i.e. from start of last month until now) is then validation data"""
-    for col in live_data.columns:
+    for col in master_feature_set_na_removed.columns:
         live_data[col] = live_data[col].astype(master_feature_set_na_removed[col].dtypes.name)
 
     last_monday = datetime.date.today() + datetime.timedelta(days=-datetime.date.today().weekday())
