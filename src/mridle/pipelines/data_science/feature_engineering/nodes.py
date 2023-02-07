4806d427b7b98fbc53deba4e82b7d659f57c8a5a
Mark McMahon <mcmahom5@tcd.ie>
2023-02-07 10:00:13 +0100

    Push

--- src/mridle/pipelines/data_science/feature_engineering/nodes.py
+++ src/mridle/pipelines/data_science/feature_engineering/nodes.py
@@ -86,8 +86,8 @@ def generate_3_5_days_ahead_features(status_df, f_dt, live_data=False):
         start_dt = add_business_days(f_dt, 3).date()
         end_dt = add_business_days(f_dt, 5).date()
 
-    pertinent_appts = fn_status_df.loc[(fn_status_df['now_sched_for_date'].date() >= start_dt) &
-                                       (fn_status_df['now_sched_for_date'].date() <= end_dt),
+    pertinent_appts = fn_status_df.loc[(fn_status_df['now_sched_for_date'].dt.date() >= start_dt) &
+                                       (fn_status_df['now_sched_for_date'].dt.date() <= end_dt),
                                        ['FillerOrderNo', 'MRNCmpdId', 'now_sched_for_date']].drop_duplicates()
 
     fn_status_df_fons = fn_status_df[
