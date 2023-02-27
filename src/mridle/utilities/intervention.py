8c39f9cad08117449c646304d628c43ed271bc33
Mark McMahon <mcmahom5@tcd.ie>
2023-02-27 15:02:38 +0100

    Send email code

---
+++ src/mridle/utilities/intervention.py
@@ -0,0 +1,48 @@
+import smtplib
+import datetime
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+import configparser
+
+# Read the configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Access the values in the configuration file
+username = config['DEFAULT']['username']
+password = config['DEFAULT']['password']
+recipients = config['DEFAULT']['recipients']
+
+# create an SMTP object
+smtp_obj = smtplib.SMTP('outlook.usz.ch', 587)
+
+# establish a secure connection
+smtp_obj.starttls()
+
+# login to the email server using your email address and password
+smtp_obj.login(username, password)
+
+# create the email message
+msg = MIMEMultipart()
+msg['From'] = username
+msg['To'] = COMMASPACE.join([recipients])
+msg['Date'] = formatdate(localtime=True)
+msg['Subject'] = 'DataFrame as csv attachment'
+
+body = "Time sent {}".format(datetime.datetime.now())
+msg.attach(MIMEText(body, 'plain'))
+
+# Add the CSV attachment to the email
+with open('/data/mridle/data/silent_live_test/my_dataframe.csv', 'rb') as csv_file:
+    csv_attachment = MIMEApplication(csv_file.read(), _subtype='csv')
+    csv_attachment.add_header('Content-Disposition', 'attachment', filename='my_dataframe.csv')
+    msg.attach(csv_attachment)
+
+
+# send the email
+smtp_obj.sendmail(msg['From'], msg['To'], msg.as_string())
+
+# close the SMTP connection
+smtp_obj.quit()
